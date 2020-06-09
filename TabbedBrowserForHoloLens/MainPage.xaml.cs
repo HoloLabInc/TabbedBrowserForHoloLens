bd1c8030dd36d8bb6a20d7052fbdbb0542b35e0c
Furuta Yusuke <furuta.yusuke.012@gmail.com>
2020-06-09 15:05:53 +0900

    first commit

---
+++ TabbedBrowserForHoloLens/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+namespace TabbedBrowserForHoloLens
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public void OpenNewPage(string uri)
+        {
+            var webView = new WebView
+            {
+                Source = new Uri(uri)
+            };
+
+            var newTab = new TabViewItem
+            {
+                Content = webView,
+
+                IconSource = new Microsoft.UI.Xaml.Controls.SymbolIconSource()
+                {
+                    Symbol = Symbol.Document
+                },
+                Header = "New Document"
+            };
+
+            tabView.TabItems.Add(newTab);
+            tabView.SelectedItem = newTab;
+        }
+    }
+}
